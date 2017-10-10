6c0caea81bc324936468a7fec558bab641adbb1a
ericyoon1 <eriqyoon@gmail.com>
2017-10-10 12:15:03 -0700

    Got Cocktail image and Cocktail name

--- script/main.js
+++ script/main.js
@@ -1,15 +1,32 @@
 var vod = "Vodka";
 var lowrat = '2.0';
 var highrat = '4.0';
+/***/
+var randomCocktailName = null;
+var randomCocktailImageSource = null;
 
 $(document).ready(function(){
 	$('#top-filler').css('height',$('nav').height());
+	$('#randomCocktailButton').on('click',handleRandomCocktailDataClick);
 	// getData().then(dataPullSuccess,dataPullFail);
 	// yelpTest();
 
 	// getRandomCocktailData().then(dataPullSuccess, dataPullFail);
 	// getDrinkByLiquor(vod).then(dataPullSuccess, dataPullFail);
 
-	getRandMovieByRating(lowrat,highrat).then(rouletteSuccess,rouletteFail);
-	getYelpData().then(yelpSuccess,yelpFail);
+	// getRandMovieByRating(lowrat,highrat).then(rouletteSuccess,rouletteFail);
+	// getYelpData().then(yelpSuccess,yelpFail);
 });
+
+function handleRandomCocktailDataClick(){
+    sendRandomCocktailData();
+}
+
+function sendRandomCocktailData(){
+    randomCocktailName = randomDrinkData.drinks[0].strDrink;
+    $('#randomCocktailDisplay').text("Drink Name: " + randomCocktailName);
+    var newImgTag = $('<img>');
+    $('#randomCocktailImgDisplay').append(newImgTag);
+    randomCocktailImageSource = randomDrinkData.drinks[0].strDrinkThumb;
+    $(newImgTag).attr('src', randomCocktailImageSource);
+}
--- script/roulette.js
+++ script/roulette.js
@@ -1,37 +1,37 @@
-function getRandMovieByRating(lowR,highR){
-	var promise = {
-		then: function(resolve, reject){
-			this.resolve = resolve;
-			this.reject = reject;
-		}
-	};
-	$.ajax({
-		url: 'https://netflixroulette.net/api/api.php',
-		dataType: 'json',
-		method: 'post',
-		data: {
-			'genre' : 'All',
-			'movies': 'true',
-			'tv' :'true',
-			'lowrating': lowR,
-			'highrating' : highR,
-			'director' :'none',
-			'actor' : 'none',
-			'keyword' : 'none'
-		},
-		success: function(showData){
-			promise.resolve(showData);
-		},
-		error: function(err){
-			promise.resolve(err);
-		}
-	});
-	return promise;
-}
-
-function rouletteSuccess(dat){
-	console.log(dat);
-}
-function rouletteFail(dat){
-	console.log(dat);
-}
+// function getRandMovieByRating(lowR,highR){
+// 	var promise = {
+// 		then: function(resolve, reject){
+// 			this.resolve = resolve;
+// 			this.reject = reject;
+// 		}
+// 	};
+// 	$.ajax({
+// 		url: 'https://netflixroulette.net/api/api.php',
+// 		dataType: 'json',
+// 		method: 'post',
+// 		data: {
+// 			'genre' : 'All',
+// 			'movies': 'true',
+// 			'tv' :'true',
+// 			'lowrating': lowR,
+// 			'highrating' : highR,
+// 			'director' :'none',
+// 			'actor' : 'none',
+// 			'keyword' : 'none'
+// 		},
+// 		success: function(showData){
+// 			promise.resolve(showData);
+// 		},
+// 		error: function(err){
+// 			promise.resolve(err);
+// 		}
+// 	});
+// 	return promise;
+// }
+//
+// function rouletteSuccess(dat){
+// 	console.log(dat);
+// }
+// function rouletteFail(dat){
+// 	console.log(dat);
+// }
