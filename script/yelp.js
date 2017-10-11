bdf08501a34754362e077d0f7dc44294f0fe4be6
briandhkim <w1057216@gmail.com>
2017-10-11 12:02:02 -0700

    yelp changes made for html

--- script/yelp.js
+++ script/yelp.js
@@ -1,3 +1,5 @@
+var zipcode = "92620"
+
 function getYelpData(){
   var promise = {
     then: function(resolve, reject){
@@ -10,7 +12,7 @@ function getYelpData(){
     method: 'POST',
     data:{
       "term" : "food",
-      "location" : "92620",
+      "location" : zipcode,
       "access_token": "QjA1zFI4n5W5lwe8R8AuoXLN2BOtgQQGxRGGEjh_SV7FDCiM7E6toU9vBpm8jRGSxlFqavss1AmTPCIkw6d0N--wGEYeo5y4eEfwD8UUT6kZ2d5k_N0dWtc7KGTcWXYx"
     },
     success: function(yelpData){
