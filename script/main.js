00c15ae5c1d34bc58f83af8245bb5bd5fa7aa5d5
ericyoon1 <eriqyoon@gmail.com>
2017-10-10 18:26:39 -0700

    got drink list by ingredients

--- script/main.js
+++ script/main.js
@@ -4,14 +4,25 @@ var highrat = '4.0';
 /***/
 var randomCocktailName = null;
 var randomCocktailImageSource = null;
+var randomCocktailInstructions = null;
 var ingredientsArray = [];
 var ingredientsArrayFiltered = [];
 var measuresArray = [];
 var measuresArrayFiltered = [];
+var drinkByIngredientArray = [];
 
 $(document).ready(function(){
 	$('#top-filler').css('height',$('nav').height());
 	$('#randomCocktailButton').on('click',handleRandomCocktailDataClick);
+	$('#vodkaButton').on('click',handleCocktailByIngredientClick);
+	$('#scotchButton').on('click',handleCocktailByIngredientClick);
+    $('#tequilaButton').on('click',handleCocktailByIngredientClick);
+    $('#ginButton').on('click',handleCocktailByIngredientClick);
+    $('#rumButton').on('click',handleCocktailByIngredientClick);
+    $('#bourbonButton').on('click',handleCocktailByIngredientClick);
+    $('#brandyButton').on('click',handleCocktailByIngredientClick);
+    $('#beerButton').on('click',handleCocktailByIngredientClick);
+
 	// getData().then(dataPullSuccess,dataPullFail);
 
 	// getRandomCocktailData().then(dataPullSuccess, dataPullFail);
@@ -24,13 +35,41 @@ $(document).ready(function(){
 
 /*****start of eric's js edit*****/
 
+
+
+/*****//*****//*****//*****//*****//*****//*****//*****//*****//*****/
+
+function handleCocktailByIngredientClick(){
+    liquor = $(this).text();
+    getCocktailByIngredient().then(dataPullSuccess,dataPullFail);
+    var drinkListButton = $('<button>').text(liquor + ' Drink List').on('click',sendCocktailByIngredient);
+    $('#drinkButtonResponseDiv').append(drinkListButton);
+}
+
+function sendCocktailByIngredient(){
+    console.log(drinkByIngredient.drinks);
+    for(var i=0; i<drinkByIngredient.drinks.length; i++){
+        drinkByIngredientArray.push(drinkByIngredient.drinks[i].strDrink);
+    }
+    console.log(drinkByIngredientArray);
+    for(var j=0; j<drinkByIngredientArray.length; j++){
+        var drinkListDiv = $('<div>');
+        $('#drinkList').append(drinkListDiv);
+        var drinkButton = $('<button>'); //onclick
+        $(drinkListDiv).append(drinkButton);
+        $(drinkButton).append(drinkByIngredientArray[j]);
+    }
+}
+
+/*****//*****//*****//*****//*****//*****//*****//*****//*****//*****/
+
 function handleRandomCocktailDataClick(){
     sendRandomCocktailData();
 }
 
 function sendRandomCocktailData(){
     randomCocktailName = randomDrinkData.drinks[0].strDrink;
-    $('#randomCocktailDisplay').text("Drink Name: " + randomCocktailName);
+    $('#randomCocktailDisplay').text('Drink Name: ' + randomCocktailName);
     var newImgTag = $('<img>').addClass('cocktailImage');
     $('#randomCocktailImgDisplay').append(newImgTag);
     randomCocktailImageSource = randomDrinkData.drinks[0].strDrinkThumb;
@@ -38,7 +77,7 @@ function sendRandomCocktailData(){
 
     randomCocktailIngredients();
     randomCocktailMeasures();
-    randomCocktailInstructions();
+    randomCocktailDirections();
 }
 
 function randomCocktailIngredients(){
@@ -53,11 +92,10 @@ function randomCocktailIngredients(){
 		}
 	}
 	for(var n=0; n<ingredientsArrayFiltered.length; n++){
-		var ingredientDiv = $('<div>');
-        $('#randomCocktailIngredientsDisplay').append(ingredientDiv);
-        $(ingredientDiv).append(ingredientsArrayFiltered[n] + " : ");
+		var ingredientsDiv = $('<div>');
+        $('#randomCocktailIngredientsDisplay').append(ingredientsDiv);
+        $(ingredientsDiv).append(ingredientsArrayFiltered[n] + " : ");
 	}
-	//console.log(randomDrinkData.drinks[0]);
 }
 
 function randomCocktailMeasures(){
@@ -72,15 +110,23 @@ function randomCocktailMeasures(){
         }
     }
     for(var n=0; n<measuresArrayFiltered.length; n++){
-        var measureDiv = $('<div>');
-        $('#randomCocktailMeasuresDisplay').append(measureDiv);
-        $(measureDiv).append(measuresArrayFiltered[n]);
+        var measuresDiv = $('<div>');
+        $('#randomCocktailMeasuresDisplay').append(measuresDiv);
+        $(measuresDiv).append(measuresArrayFiltered[n]);
     }
-    //console.log(randomDrinkData.drinks[0]);
 }
 
-function randomCocktailInstructions(){
+function randomCocktailDirections(){
+    var underlineTag = $('<u></u>');
+    $('#instructionsHeader').append(underlineTag);
+    var newHeader = $('<h4>');
+    $(underlineTag).append(newHeader);
+    $(newHeader).text('Instructions: ');
 	var instructionsDiv = $('<div>');
+	$('#randomCocktailInstructionsDisplay').append(instructionsDiv);
+	randomCocktailInstructions = randomDrinkData.drinks[0].strInstructions;
+	$(instructionsDiv).text(randomCocktailInstructions);
 }
+
 /*****end of eric's js edit*****/
 
