e9198eeff115701eeba95913408b14f57c6e8ae3
briandhkim <w1057216@gmail.com>
2017-10-11 17:16:11 -0700

    cocktail api edit

--- script/cocktail.js
+++ script/cocktail.js
@@ -1,9 +1,8 @@
-var liquor = null;
-var cocktail = null;
-var randomDrinkData = null;
-var drinkByIngredient = null;
-var specificDrinkData = null;
+var liquor = 'Vodka';   //change back to null
+var cocktail = '';   //change back to null
 
+
+// getRandomCocktailData().then(randCocktailSuccess,randCocktailFail);
 function getRandomCocktailData(){
 	var promise = {
 		then: function(resolve, reject){
@@ -25,16 +24,28 @@ function getRandomCocktailData(){
 	});
 	return promise;
 }
-function dataPullSuccess(drink_data){
-	console.log(drink_data);
-
+/**
+    drink_data object with key[drinks] and val array(holds data)
+            drink_data.drinks: [{}]
+            drink_data.drinks[0] = {}
+            drink_data.drinks[0].strDrink = "drink name"
+            drink_data.drinks[0].strDrinkThumb = 'img src'
+            drink_data.drinks[0].strInstructions = 'instructions'
+            drink_data.drinks[0].strIngredient1 = 'first ingredient'    ;will need to be looped to check all (1-15) -returns empty string for blank
+            drink_data.drinks[0].strMeasure1 = 'first ing measure'      ;will have same idx as strIng
+**/
+function randCocktailSuccess(drink_data){
+    // console.log(drink_data.drinks[0]);
+    // console.log(drink_data.drinks[0].strIngredient12);
+    var ct_data = drink_data.drinks[0];
+    drinkDOMupdate(ct_data);            //call the drinkDOMupdate function with drink data object 
 }
-function dataPullFail(err){
+function randCocktailFail(err){
 	console.log(err);
 }
 
-// getRandomCocktailData().then(dataPullSuccess,dataPullFail);
 
+// getCocktailByIngredient().then(cocktailIngSuccess,cocktailIngFail);
 function getCocktailByIngredient(){
     var promise = {
         then: function(resolve, reject){
@@ -56,14 +67,30 @@ function getCocktailByIngredient(){
     });
     return promise;
 }
-function dataPullSuccess(drink_data){
-    console.log(drink_data);
+/**
+    drink_data.drinks = []      -array length differes everytime
+    choose one drink by getting a number based on array length
+    only returns cocktail name. need to do getSpecificCocktailData with cocktail name
+    cocktail(globalvariable) gets assigned (up top) --> name of the drink in string
+
+    this function randomly selects from given array of drinks based on liquor selection
+    calls ajax call function for getting a drink based on cocktail name
+**/
+function cocktailIngSuccess(drink_data){
+    // console.log(drink_data);
+    // console.log(drink_data.drinks.length); 
+    var i = Math.floor(Math.random()*drink_data.drinks.length);
+    var randDrink = drink_data.drinks.splice(i,1);
+    // console.log(randDrink[0].strDrink);
+    cocktail = randDrink[0].strDrink;
+    getSpecificCocktailData().then(cocktailSpecificSuccess,cocktailSpecificFail);
 }
-function dataPullFail(err){
+function cocktailIngFail(err){
     console.log(err);
 }
 
-// getCocktailByIngredient().then(dataPullSuccess,dataPullFail);
+
+
 
 function getSpecificCocktailData(){
     var promise = {
@@ -87,21 +114,68 @@ function getSpecificCocktailData(){
     });
     return promise;
 }
-function dataPullSuccess(drink_data){
+/**
+    same data returned as getRandomCocktailData
+**/ 
+function cocktailSpecificSuccess(drink_data){
     console.log(drink_data);
+     var ct_data = drink_data.drinks[0];
+    drinkDOMupdate(ct_data);        //call the drinkDOMupdate function with drink data object 
 }
-function dataPullFail(err){
+function cocktailSpecificFail(err){
     console.log(err);
 }
 
-function sendSpecificCocktailData(){
-    var specificCocktailName = specificDrinkData.drinks[0].strDrink;
-    $('.drinkName').text(specificCocktailName);
-    var specificCocktailImageSource = specificDrinkData.drinks[0].strDrinkThumb;
-    $('.randomCocktailImg').attr('src', specificCocktailImageSource);
-    specificCocktailIngredients();
-    specificCocktailMeasures();
-    specificCocktailDirections();
+
+
+
+function cocktailButton(){
+    var userInput = $('#drinkSelect').val();
+    // console.log(userInput)
+    if (userInput==="Random"){
+        // do random drink ajax call
+        getRandomCocktailData().then(randCocktailSuccess,randCocktailFail);
+    }else {
+        liquor = userInput;
+        getCocktailByIngredient().then(cocktailIngSuccess, cocktailIngFail);
+        // do random drink ajax call
+    }
 }
 
-// getSpecificCocktailData().then(dataPullSuccess,dataPullFail);
+/**
+    updates dom with drink info passed in
+    param - drink_data.drinks[0]  = object with drink data reference the comment up top
+    return - nothing
+**/
+function drinkDOMupdate(ct_data){   
+ // saving ingredient and measures data
+    var cocktailIngredients = [];
+    var cocktailIngMeasures = [];
+    for (var i=1; i<16; i++){
+        if(ct_data['strIngredient'+i]!==''){
+            cocktailIngredients[i-1] = ct_data['strIngredient'+i];
+            cocktailIngMeasures[i-1] = ct_data['strMeasure'+i];
+        }
+    }
+    // console.log(cocktailIngredients, cocktailIngMeasures);
+    //end of saving ingredient and measures data
+
+    $('.drinkName').text(ct_data.strDrink);
+    $('.cocktailInstructions').text(ct_data.strInstructions);
+    $('.randomCocktailImg').attr('src', ct_data.strDrinkThumb);
+    $('.ingredientsList li').remove();
+    var ingList = $('.ingredientsList');
+    for(var i=0; i<cocktailIngredients.length; i++){
+        var ing  = $('<span>', {
+            text: cocktailIngredients[i] + ' : ',
+            class : 'ct-Ingredient'
+        });
+        var measure = $('<span>',{
+            text: cocktailIngMeasures[i],
+            class : 'ct-Measure'
+        });
+
+        var liElm = $('<li>').append(ing).append(measure);
+        ingList.append(liElm);
+    }
+}
